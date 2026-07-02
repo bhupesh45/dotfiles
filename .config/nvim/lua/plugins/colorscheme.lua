f4229cefe43132c757101ffb5e7b909fa1b3194d
Bhupesh Rawat <154693000+bhupesh45@users.noreply.github.com>
2026-07-02 19:45:17 +0530

    Updated nvim colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,71 @@
 -- return {
+-- 	"folke/tokyonight.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+--
+-- 	opts = {
+-- 		style = "storm", -- storm | moon | night | day
+-- 		transparent = false,
+-- 		terminal_colors = true,
+--
+-- 		styles = {
+-- 			comments = { italic = true },
+-- 			keywords = { italic = true },
+-- 			functions = {},
+-- 			variables = {},
+-- 			sidebars = "dark",
+-- 			floats = "dark",
+-- 		},
+--
+-- 		sidebars = {
+-- 			"qf",
+-- 			"help",
+-- 			"terminal",
+-- 			"neo-tree",
+-- 			"lazy",
+-- 			"mason",
+-- 			"Trouble",
+-- 		},
+--
+-- 		hide_inactive_statusline = false,
+-- 		dim_inactive = false,
+-- 		lualine_bold = true,
+--
+-- 		on_highlights = function(hl, c)
+-- 			-- Cursor line
+-- 			hl.CursorLine = { bg = c.bg_highlight }
+--
+-- 			-- Line numbers
+-- 			hl.LineNr = { fg = c.dark5 }
+-- 			hl.CursorLineNr = { fg = c.orange, bold = true }
+--
+-- 			-- Floating windows
+-- 			hl.NormalFloat = { bg = c.bg_dark }
+-- 			hl.FloatBorder = { fg = c.blue, bg = c.bg_dark }
+--
+-- 			-- Telescope
+-- 			hl.TelescopeBorder = { fg = c.blue, bg = c.bg_dark }
+-- 			hl.TelescopePromptBorder = { fg = c.green, bg = c.bg_dark }
+--
+-- 			-- Popup menu
+-- 			hl.Pmenu = { bg = c.bg_dark }
+-- 			hl.PmenuSel = { bg = c.bg_highlight }
+--
+-- 			-- Visual selection
+-- 			hl.Visual = { bg = c.bg_visual }
+--
+-- 			-- Search
+-- 			hl.Search = { bg = c.yellow, fg = c.black }
+-- 			hl.IncSearch = { bg = c.orange, fg = c.black }
+-- 		end,
+-- 	},
+--
+-- 	config = function(_, opts)
+-- 		require("tokyonight").setup(opts)
+-- 		vim.cmd.colorscheme("tokyonight")
+-- 	end,
+-- }
+-- return {
 --     "Skardyy/makurai-nvim",
 --     lazy = false,
 --     priority = 1000,
@@ -30,76 +97,93 @@
 --     end,
 -- }
 
+return {
+	"tiagovla/tokyodark.nvim",
+	lazy = false,
+	priority = 1000,
+
+	opts = {
+		transparent_background = true,
+		gamma = 1.0,
+		terminal_colors = true,
+
+		styles = {
+			comments = { italic = true },
+			keywords = { italic = true },
+			identifiers = { italic = false },
+			functions = {},
+			variables = {},
+		},
+
+		custom_highlights = {},
+		custom_palette = {},
+	},
+
+	config = function(_, opts)
+		require("tokyodark").setup(opts)
+		vim.cmd.colorscheme("tokyodark")
+	end,
+}
+
 -- return {
---     "tiagovla/tokyodark.nvim",
---     opts = {
---         -- custom options here
---     },
---     config = function(_, opts)
---         require("tokyodark").setup(opts) -- calling setup is optional
---         vim.cmd [[colorscheme tokyodark]]
+--     "loctvl842/monokai-pro.nvim",
+--     config = function()
+--         require("monokai-pro").setup {
+--             transparent_background = false,
+--             terminal_colors = true,
+--             devicons = true, -- highlight the icons of `nvim-web-devicons`
+--             styles = {
+--                 comment = { italic = true },
+--                 keyword = { italic = true }, -- any other keyword
+--                 type = { italic = true }, -- (preferred) int, long, char, etc
+--                 storageclass = { italic = true }, -- static, register, volatile, etc
+--                 structure = { italic = true }, -- struct, union, enum, etc
+--                 parameter = { italic = true }, -- parameter pass in function
+--                 annotation = { italic = true },
+--                 tag_attribute = { italic = true }, -- attribute of tag in reactjs
+--             },
+--             filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+--             -- Enable this will disable filter option
+--             day_night = {
+--                 enable = false, -- turn off by default
+--                 day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+--                 night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
+--             },
+--             inc_search = "background", -- underline | background
+--             background_clear = {
+--                 "float_win",
+--                 "toggleterm",
+--                 "telescope",
+--                 -- "which-key",
+--                 "renamer",
+--                 "notify",
+--                 -- "nvim-tree",
+--                 -- "neo-tree",
+--                 -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
+--             }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
+--             plugins = {
+--                 bufferline = {
+--                     underline_selected = false,
+--                     underline_visible = false,
+--                 },
+--                 indent_blankline = {
+--                     context_highlight = "default", -- default | pro
+--                     context_start_underline = false,
+--                 },
+--             },
+--             overrideScheme = function(cs, p, config, hp)
+--                 local cs_override = {}
+--                 local calc_bg = hp.blend(p.background, 0.5, "#000000")
+--
+--                 cs_override.editor = {
+--                     background = calc_bg,
+--                 }
+--                 return cs_override
+--             end,
+--         }
+--         vim.cmd.colorscheme "monokai-pro"
 --     end,
 -- }
-
-return {
-    "loctvl842/monokai-pro.nvim",
-    config = function()
-        require("monokai-pro").setup {
-            transparent_background = false,
-            terminal_colors = true,
-            devicons = true, -- highlight the icons of `nvim-web-devicons`
-            styles = {
-                comment = { italic = true },
-                keyword = { italic = true }, -- any other keyword
-                type = { italic = true }, -- (preferred) int, long, char, etc
-                storageclass = { italic = true }, -- static, register, volatile, etc
-                structure = { italic = true }, -- struct, union, enum, etc
-                parameter = { italic = true }, -- parameter pass in function
-                annotation = { italic = true },
-                tag_attribute = { italic = true }, -- attribute of tag in reactjs
-            },
-            filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
-            -- Enable this will disable filter option
-            day_night = {
-                enable = false, -- turn off by default
-                day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
-                night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
-            },
-            inc_search = "background", -- underline | background
-            background_clear = {
-                "float_win",
-                "toggleterm",
-                "telescope",
-                -- "which-key",
-                "renamer",
-                "notify",
-                -- "nvim-tree",
-                -- "neo-tree",
-                -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
-            }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
-            plugins = {
-                bufferline = {
-                    underline_selected = false,
-                    underline_visible = false,
-                },
-                indent_blankline = {
-                    context_highlight = "default", -- default | pro
-                    context_start_underline = false,
-                },
-            },
-            overrideScheme = function(cs, p, config, hp)
-                local cs_override = {}
-                local calc_bg = hp.blend(p.background, 0.5, "#000000")
-
-                cs_override.editor = {
-                    background = calc_bg,
-                }
-                return cs_override
-            end,
-        }
-        vim.cmd.colorscheme "monokai-pro"
-    end,
-}
 
 -- return {
 --     "loctvl842/monokai-pro.nvim",
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,57 +1,60 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    -- event = "VeryLazy",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-        options = {
-            icons_enabled = true,
-            theme = "auto",
-            -- component_separators = { left = "", right = "" },
-            component_separators = { left = "|", right = "|" },
-            -- section_separators = { left = "", right = "" },
-            section_separators = { left = "", right = "" },
-            disabled_filetypes = {
-                statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" },
-                winbar = {},
-            },
-            ignore_focus = {},
-            always_divide_middle = true,
-            always_show_tabline = true,
-            globalstatus = true,
-            refresh = {
-                statusline = 100,
-                tabline = 100,
-                winbar = 100,
-            },
-        },
-        sections = {
-            lualine_a = { "mode" },
-            lualine_b = { "branch", "diff", "diagnostics" },
-            lualine_c = { "filename" },
-            lualine_x = {
-                "encoding",
-                "fileformat",
-                "filetype",
-                {
-                    require("noice").api.statusline.mode.get,
-                    cond = require("noice").api.statusline.mode.has,
-                    color = { fg = "#ff9e64" },
-                },
-            },
-            lualine_y = { "progress" },
-            lualine_z = { "location" },
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = { "filename" },
-            lualine_x = { "location" },
-            lualine_y = {},
-            lualine_z = {},
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {},
-    },
+	"nvim-lualine/lualine.nvim",
+	event = { "BufReadPost", "BufNewFile" },
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "auto",
+			component_separators = { left = "|", right = "|" },
+			section_separators = { left = "", right = "" },
+			disabled_filetypes = {
+				statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" },
+				winbar = {},
+			},
+			ignore_focus = {},
+			always_divide_middle = true,
+			always_show_tabline = true,
+			globalstatus = true,
+			refresh = {
+				statusline = 100,
+				tabline = 100,
+				winbar = 100,
+			},
+		},
+		sections = {
+			lualine_a = {
+				-- Explicitly declare BOTH left and right to keep the inner curve
+				{ "mode", separator = { left = "", right = "" }, padding = 1 },
+			},
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { "filename" },
+			lualine_x = {
+				"encoding",
+				"fileformat",
+				"filetype",
+				{
+					require("noice").api.statusline.mode.get,
+					cond = require("noice").api.statusline.mode.has,
+					color = { fg = "#ff9e64" },
+				},
+			},
+			lualine_y = { "progress" },
+			lualine_z = {
+				-- Explicitly declare BOTH left and right to cap the end perfectly
+				{ "location", separator = { left = "", right = "" }, padding = 1 },
+			},
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { "filename" },
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		winbar = {},
+		inactive_winbar = {},
+		extensions = {},
+	},
 }
